718614bdb7ec626c494168beb306d7f9ab72292f
ashritaravishankar <77598737+ashritaravishankar@users.noreply.github.com>
2021-11-13 22:49:02 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+function preload(){
+
+}
+
+function setup(){
+    canvas = createCanvas(400,350);
+    canvas.center();
+    
+
+  
+}
+
+function draw(){
+
+
+    
+
+}
+
+function take_photo(){
+    save('My_Lipstick_Filter_Image.png');
+
+}
